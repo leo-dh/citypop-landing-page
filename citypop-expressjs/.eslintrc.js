898e4a5712a314f13e311b3a0bed3025a8cf2180
Leo Ding Hao <leo.ding.hao.2014@gmail.com>
2020-07-29 18:00:15 +0800

    init

---
+++ citypop-expressjs/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module"
+  },
+  env: {
+    node: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ]
+};
